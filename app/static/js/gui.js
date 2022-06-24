ee58273cd8619baed1ccbef5d61aaf95de9f54f6
Austin Hoag <austinthomashoag@gmail.com>
2022-06-24 09:11:02 -0700

    can delete constraints

--- app/static/js/gui.js
+++ app/static/js/gui.js
@@ -53,18 +53,32 @@ function addConstraintField() {
 	// Add another target_grid_container div
 
 	// First figure out how many target grid containers are already in the document
-	let target_grid_containers = document.querySelectorAll('.target_grid_container');
-	let last_target_grid_container = target_grid_containers[target_grid_containers.length -1]
-	let parentDiv = last_target_grid_container.parentNode
-	let n_containers = target_grid_containers.length
-	let new_container_id = "target_grid_container" + (n_containers+1)
-	newHeader = document.createElement('h5')
-	newHeader.classList.add('mt-4')
-	newHeader.textContent = "Constraint #" + (n_containers+1) + ":"
+	let constraint_containers = document.querySelectorAll('.constraint_container');
+	let last_constraint_container = constraint_containers[constraint_containers.length - 1]
+	let parentNode = last_constraint_container.parentNode
+	let n_constraints = constraint_containers.length
+	let new_container_id = "constraint" + (n_constraints+1)
 
-	newDiv = document.createElement('div');
-  newDiv.classList.add('target_grid_container');
-  newDiv.id = new_container_id;
+	newConstraintDiv = document.createElement('div');
+  newConstraintDiv.id = new_container_id;
+  newConstraintDiv.classList.add('constraint_container')
+
+	newHeader = document.createElement('h5')
+	newHeader.classList.add('mt-2')
+	newHeader.textContent = "Constraint #" + (n_constraints+1) + ":"
+
+	deleteConstraintButton = document.createElement('button')
+  deleteConstraintButton.type = "button"
+  deleteConstraintButton.classList.add('btn','btn-danger','deleteconstraint-btn')
+  deleteConstraintButton.addEventListener('click', function(){
+      deleteConstraint(this);
+  });
+  
+  deleteConstraintButton.textContent = "Remove this constraint"
+
+	targetDiv = document.createElement('div');
+  targetDiv.classList.add('target_grid_container');
+  targetDiv.id = new_container_id;
 
   newTargetDiv = document.createElement('div');
   newTargetDiv.classList.add('newtargetbox');
@@ -80,18 +94,30 @@ function addConstraintField() {
   newRemoveDiv.classList.add('deletetargetbox','ml-4');
   newRemoveDiv.textContent = "Drag block here to remove"
 
+  targetDiv.appendChild(newTargetDiv)
+  targetDiv.appendChild(newTextDiv)
+  targetDiv.appendChild(newRemoveDiv)
 
-  newDiv.appendChild(newTargetDiv)
-  newDiv.appendChild(newTextDiv)
-  newDiv.appendChild(newRemoveDiv)
+  newConstraintDiv.appendChild(newHeader)
+  newConstraintDiv.appendChild(targetDiv)
+  newConstraintDiv.appendChild(deleteConstraintButton)
 
-  // Get the form as the parent to add this newDiv to
-  // let form = document.forms[0]
-  parentDiv.insertBefore(newDiv, last_target_grid_container.nextSibling);
-  parentDiv.insertBefore(newHeader, newDiv);
-		
+  parentNode.insertBefore(newConstraintDiv, last_constraint_container.nextSibling);
 	};
 
+function deleteConstraint(elem) {
+		elem.parentNode.remove();
+		// Re-number the constraints - amounts to changing the header names 
+		// and the id of the constraint_container
+		let constraint_containers = document.querySelectorAll('.constraint_container');
+		for (let i=0, iLen=constraint_containers.length; i<iLen; i++) {
+			cc = constraint_containers[i]
+			cc.id = "constraint" + (i+1)
+			header = cc.querySelector('h5')
+			header.textContent = "Constraint #" + (i+1) + ":"
+		};
+
+	}
 
 window.onclick = function(event) {
 	// Handle when user clicks away from a dropdown or any of its items
